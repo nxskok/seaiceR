1ec691b63c1f495dad370b6da653989540882d76
Ken Butler <ken@swiss-soccer.net>
2019-08-28 11:40:21 -0400

    write make_long

--- R/make_long.R
+++ R/make_long.R
@@ -1,2 +1,16 @@
 #' Make wide data long
 #'
+#' @param d data frame in wide format (years in column 1, called `year`, locations in other columns)
+#' @return data frame in long format with one row per observation
+#'
+#' @author Ken Butler, \email{butler@utsc.utoronto.ca}
+#'
+#' @examples
+make_long(example_data)
+#' @export
+#'
+make_long=function(d) {
+  d %>% tidyr::gather(location, day, -year) %>%
+    dplyr::mutate(location=as.numeric(location)) -> d_long
+  d_long
+}
---
+++ data-raw/DATASET.R
@@ -0,0 +1,3 @@
+## code to prepare `DATASET` dataset goes here
+example_data <- readRDS(url("http://www.utsc.utoronto.ca/~butler/sea_ice_data.rds"))
+usethis::use_data("example_data")
